49cc68bdd4521d09f521f449073cee0f334b7107
Spencer Hayes <113189856+spencerschoolaccount@users.noreply.github.com>
2023-01-12 15:50:48 +0000

    Cards can now be discarded

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ def gameloop():
 	opponentSlot5 = [False, playSlot5[1]]
 
 	fullHandMessageTime = 0
+	discardMode = False
 	
 	gameExit = False
 	while not gameExit:
@@ -63,14 +64,54 @@ def gameloop():
 				gameExit = True
 
 			elif event.type == pygame.MOUSEBUTTONUP:
-				if event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(1098,1098+cardWidth) and pygame.mouse.get_pos()[1] in range(360, 360+cardHeight):
-					for i in range(1,7):
-						if not locals()['handSlot' + str(i)][0]:
-							locals()['handSlot' + str(i)][0] = True
-							break
-						else:
-							if i == 6:
-								fullHandMessageTime = 2 * FPS
+				if not discardMode:
+					if event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(1098,1098+cardWidth) and pygame.mouse.get_pos()[1] in range(360, 360+cardHeight):
+						for i in range(1,7):
+							if not locals()['handSlot' + str(i)][0]:
+								locals()['handSlot' + str(i)][0] = True
+								break
+							else:
+								if i == 6:
+									fullHandMessageTime = 2 * FPS
+	
+					elif event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(58,58+cardWidth) and pygame.mouse.get_pos()[1] in range(360, 360+cardHeight):
+						discardMode = True
+
+					elif (mouseX = pygame.mouse.get_pos()[0]) (mouseY = pygame.mouse.get_pos()[1]) event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(handSlot[1],handSlot6[1] + cardWidth) and pygame.mouse.get_pos()[1] in range(handSlot1[2], handSlot1[2] + cardHeight):
+						if mouseX in range(handSlot1[1],handSlot1[1] + cardWidth):
+							
+							
+						elif mouseX in range(handSlot2[1],handSlot2[1] + cardWidth):
+
+						elif mouseX in range(handSlot3[1],handSlot3[1] + cardWidth):
+
+						elif mouseX in range(handSlot4[1],handSlot4[1] + cardWidth):
+
+						elif mouseX in range(handSlot5[1],handSlot5[1] + cardWidth):
+
+						elif mouseX in range(handSlot6[1],handSlot6[1] + cardWidth):
+						
+				else:
+					if event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(58,58+cardWidth) and pygame.mouse.get_pos()[1] in range(360, 360+cardHeight):
+						discardMode = False
+						
+					elif event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(handSlot1[1],handSlot1[1]+cardWidth) and pygame.mouse.get_pos()[1] in range(handSlot1[2], handSlot1[2]+cardHeight):
+						handSlot1[0] = False
+					
+					elif event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(handSlot2[1],handSlot2[1]+cardWidth) and pygame.mouse.get_pos()[1] in range(handSlot2[2], handSlot2[2]+cardHeight):
+						handSlot2[0] = False
+
+					elif event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(handSlot3[1],handSlot3[1]+cardWidth) and pygame.mouse.get_pos()[1] in range(handSlot3[2], handSlot3[2]+cardHeight):
+						handSlot3[0] = False
+
+					elif event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(handSlot4[1],handSlot4[1]+cardWidth) and pygame.mouse.get_pos()[1] in range(handSlot4[2], handSlot4[2]+cardHeight):
+						handSlot4[0] = False
+
+					elif event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(handSlot5[1],handSlot5[1]+cardWidth) and pygame.mouse.get_pos()[1] in range(handSlot5[2], handSlot5[2]+cardHeight):
+						handSlot5[0] = False
+
+					elif event.button == pygame.BUTTON_LEFT and pygame.mouse.get_pos()[0] in range(handSlot6[1],handSlot6[1]+cardWidth) and pygame.mouse.get_pos()[1] in range(handSlot6[2], handSlot6[2]+cardHeight):
+						handSlot6[0] = False
 		
 		gameDisplay.fill(backgroundColour)
 		pygame.draw.rect(gameDisplay, playMat, (240,20,800,474))
@@ -88,6 +129,7 @@ def gameloop():
 		pygame.draw.rect(gameDisplay, backgroundColour, (playSlot5[1], playerCardY, cardWidth, cardHeight))
 
 		pygame.draw.rect(gameDisplay, blue, (1098, 360, cardWidth, cardHeight))
+		pygame.draw.rect(gameDisplay, green, (58, 360, cardWidth, cardHeight))
 
 		if handSlot1[0]:
 			pygame.draw.rect(gameDisplay, red, (handSlot1[1], handSlot1[2], cardWidth, cardHeight))
